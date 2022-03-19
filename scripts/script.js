d7fd48b629dad970279395e0afd770a3799ddeed
Ahmed Saad <ahmed.saad.kader@gmail.com>
2022-03-19 22:08:12 +0200

    add drawing through hovering

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,22 @@
 const divContainer = document.getElementById('container-div')
 
+function drawBoard () {
     for (let index = 0; index < 256; index++){
         const gridDiv = document.createElement('div')
         gridDiv.setAttribute('class', 'grid')
+        gridDiv.setAttribute('id', 'grid-'+index)
         gridDiv.innerText = index
         divContainer.append(gridDiv)        
     }
+}
+
+drawBoard()
+
+let gridDivElement = document.getElementsByClassName('grid')
+
+for (grid in gridDivElement){
+    let gridItem = document.getElementById('grid-'+grid)
+    gridItem.addEventListener('mouseover', () => {
+        gridItem.setAttribute('class', 'grid grid-drawn')
+    })
+}
