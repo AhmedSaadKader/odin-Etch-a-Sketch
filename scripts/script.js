dd57718479050166c00410f87d6240a2b23846b9
Ahmed Saad <ahmed.saad.kader@gmail.com>
2022-03-19 20:07:42 +0200

    grid and sidebar using flexbox

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,8 @@
 const divContainer = document.getElementById('container-div')
 
-    for (let index = 0; index < 16; index++) {
+    for (let index = 0; index < 256; index++){
         const gridDiv = document.createElement('div')
-        gridDiv.setAttribute('class', 'grid grid-div')
+        gridDiv.setAttribute('class', 'grid')
         gridDiv.innerText = index
         divContainer.append(gridDiv)        
-    }
-    for (let index = 0; index < 15; index++) {
-        for (let index = 0; index < 16; index++) {
-            const gridDivBelow = document.createElement('div')
-            gridDivBelow.setAttribute('class', 'grid grid-div-below')
-            gridDivBelow.innerText = index
-            divContainer.append(gridDivBelow)        
-        }
     }
