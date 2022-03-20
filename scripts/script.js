36ec58e8283222c9a2be815274149772718ed285
Ahmed Saad <ahmed.saad.kader@gmail.com>
2022-03-20 20:05:17 +0200

    styling and buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,76 @@
 const divContainer = document.getElementById('container-div')
+const clearButton = document.getElementById('clear-board')
+const drawButton = document.getElementById('draw-board')
+const eraserButton = document.getElementById('eraser-button')
+const inputSizeElement = document.getElementById('size-input')
+let gridDivElement
 
-function drawBoard () {
-    for (let index = 0; index < 256; index++){
+function createBoard (size) {
+    let widthAndHeight = 960 / size
+    let child = divContainer.lastElementChild;
+    while(child){
+        divContainer.removeChild(child)
+        child = divContainer.lastElementChild;
+    }
+    for (let index = 0; index < size*size; index++){
         const gridDiv = document.createElement('div')
         gridDiv.setAttribute('class', 'grid')
         gridDiv.setAttribute('id', 'grid-'+index)
-        gridDiv.innerText = index
-        divContainer.append(gridDiv)        
+        gridDiv.style.width = `${widthAndHeight}px`
+        gridDiv.style.height = `${widthAndHeight}px`
+        // gridDiv.innerText = index
+        divContainer.append(gridDiv)
+        gridDivElement = document.getElementsByClassName('grid')
+    }
+    return gridDivElement
+}
+
+const clearBoard = function () {
+    for (grid in gridDivElement){
+        let gridItem = document.getElementById('grid-'+grid)
+        if (gridItem){
+            gridItem.setAttribute('class', 'grid')
+        }
     }
 }
 
-drawBoard()
-
-let gridDivElement = document.getElementsByClassName('grid')
-
-for (grid in gridDivElement){
-    let gridItem = document.getElementById('grid-'+grid)
-    gridItem.addEventListener('mouseover', () => {
-        gridItem.setAttribute('class', 'grid grid-drawn')
-    })
+const drawHover = function () {
+    for (grid in gridDivElement){
+        let gridItem = document.getElementById('grid-'+grid)
+        if (gridItem){
+            gridItem.addEventListener('mouseover', () => {
+            gridItem.setAttribute('class', 'grid grid-drawn')
+            })
+        }
+    }
 }
+
+const eraseHover = function () {
+    for (grid in gridDivElement){
+        let gridItem = document.getElementById('grid-'+grid)
+        if (gridItem){
+            gridItem.addEventListener('mouseover', () => {
+            gridItem.setAttribute('class', 'grid')
+            })
+        }
+    }
+}
+
+window.onload = function () {
+    createBoard(16)
+    drawHover()
+}
+
+clearButton.addEventListener('click', () => {
+    clearBoard()
+})
+
+drawButton.addEventListener('click', () => {
+    let size = inputSizeElement.value
+    createBoard(size)
+    drawHover()
+})
+
+eraserButton.addEventListener('click', ()=>{
+    eraseHover()
+})
